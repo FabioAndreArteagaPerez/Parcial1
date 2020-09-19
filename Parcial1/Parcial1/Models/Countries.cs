d93c61d910a86cd5ad3afdd8c3fdc8b64ad909a0
Fabio Andre Arteaga Perez <fabioandrearteagaperez@gmail.com>
2020-09-19 13:07:23 -0400

    Models

---
+++ Parcial1/Parcial1/Models/Countries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Parcial1.Models
+{
+    public class Countries
+    {
+        [Required(ErrorMessage ="You must enter this field {0}")]
+        public string alpha3code { get; set; }
+
+        [Required(ErrorMessage = "You must enter this field {0}")]
+        public string region { get; set; }
+
+        [Key]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "You must enter this field {0}")]
+        public int area { get; set; }
+
+        [Required(ErrorMessage = "You must enter this field {0}")]
+        public int callingcodes { get; set; }
+      
+    }
+}
---
+++ Parcial1/Parcial1/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Parcial1.Models
+{
+    public class DataContext:DbContext
+
+    {
+    }
+}
