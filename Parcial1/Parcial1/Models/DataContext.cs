8107d67cf44e8a4d2337ecbdf0c5cff33d183843
Fabio Andre Arteaga Perez <fabioandrearteagaperez@gmail.com>
2020-09-19 13:33:39 -0400

    Controllers

--- Parcial1/Parcial1/Models/DataContext.cs
+++ Parcial1/Parcial1/Models/DataContext.cs
@@ -7,7 +7,12 @@ using System.Web;
 namespace Parcial1.Models
 {
     public class DataContext:DbContext
-
     {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Parcial1.Models.Countries> Countries { get; set; }
     }
 }
